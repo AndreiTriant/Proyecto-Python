0776b04815af0e84fc8cb4ea73d5aee775c8c5ab
AndreiTriant <andreiatriant@gmail.com>
2026-03-06 19:33:40 +0100

    Incluir frontend como carpeta normal (no submódulo)

---
+++ dietas_frontend/src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const API_URL = "http://127.0.0.1:8080";
+
+function App() {
+  const [autor, setAutor] = useState(null);
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/autor`)
+      .then((res) => res.json())
+      .then((data) => setAutor(data.Autor))
+      .catch(() => setAutor(null));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hola mundo 2</h1>
+      {autor && <p>Autor: {autor}</p>}
+    </div>
+  );
+}
+
+export default App;
